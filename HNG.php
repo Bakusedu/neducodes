48128cd87ea1cf2e5cbb17ac2beee6fa3010e303
bakuscostan <iamnedu912@gmail.com>
2018-04-09 13:05:22 +0100

    my html page

---
+++ HNG.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html>
+ <head>
+ 	<title>Hotels.ng |HNG</title>
+ 	<style>
+ 		.Position {
+ 			text-align:center;
+ 			position:relative;
+ 			top:30px;
+ 		}
+ 	  .headerDiv {
+ 	  	background-color:#72ADFC;
+ 	  	width:800px;
+ 	  	height:80px;
+ 	  	margin-left:260px;
+ 	  	border-radius:50px;
+ 	  }
+ 	  h1 {
+ 	  	color:white;
+ 	  	text-align:center;
+ 	  	padding-top:18px;
+ 	  	font-family:sans-serif;
+ 	  }
+ 	  .Ptag {
+ 	  	color:#72ADFC;
+ 	  	font-size:1.7em;
+ 	  
+ 	  }
+ 	  .special{
+ 	  	height:200px;
+ 	  	position:relative;
+ 	  	top:100px;
+ 	  }
+ 	  .height{
+ 	  	text-align:center;
+ 			position:relative;
+ 			top:40px;
+ 	  }
+ 	  .php{
+ 	  	text-align:center;
+ 	  	font-size:5.2em;
+ 	  	color: white;
+ 	  	position:relative;
+ 	  	top:50px;
+
+ 	  }
+ 	  .SecondHDiv{
+ 	  	background-color:#72ADFC;
+ 	  	width:800px;
+ 	  	height:200px;
+ 	  	margin-left:260px;
+ 	  	border-radius:50px;
+ 	  }
+ 	</style>
+ </head>
+ <body>
+ 	<div class="headerDiv">
+ 		<h1>Hotels.ng</h1>
+ 	</div>
+ 	<div class="Position">
+ 		<p><span class="Ptag">I Hope This Design Serves!</span></p>
+ 	</div>
+ 	<div class="SecondHDiv">
+ 		<p class="php"><?php echo Date('h:i A'); ?></p>
+ 	</div>
+ 	<div class="height">
+ 		<p><span class="Ptag">#Stage-One</span></p>
+ 	</div>
+ </body>
+</html>
